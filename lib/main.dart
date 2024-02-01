85dddaf9a693c6d92a314e41738cd7431913d1b0
macstore.uz <muhammadsaidcp@gmail.com>
2024-02-01 21:19:13 +0500

    print

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,18 @@ class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
   late AnimationController animationController;
   late Tween<double> animation;
   @override
-void initState() {
-    animationController = AnimationController(vsync: this,duration:  const Duration(milliseconds: 250))
+  void initState() {
+    print(' ');
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 250))
       ..addStatusListener((status) {
-      if(status==AnimationStatus.completed){
-        animationController.reverse();
-      }
-      else if(status==AnimationStatus.dismissed){
-        animationController.forward();
-      }
-    }
-    );
-    animation = Tween<double>(begin: 0.8,end: 0);
+        if (status == AnimationStatus.completed) {
+          animationController.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          animationController.forward();
+        }
+      });
+    animation = Tween<double>(begin: 0.8, end: 0);
     animationController.forward();
     super.initState();
   }
@@ -37,64 +37,65 @@ void initState() {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // body: LayoutBuilder(builder: (context, constraints) {
-        //   print(constraints.maxHeight);
-        //   print(constraints.minHeight);
-        //   print(constraints.maxWidth);
-        //   print(constraints.minWidth);
-        //   return GridView(
-        //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        //       crossAxisCount: getMaxColumSize(constraints.maxWidth),
-        //     ),
-        //     children: [
-        //       WCard(),
-        //       WCard(),
-        //       WCard(),
-        //       WCard(),
-        //       WCard(),
-        //       WCard(),
-        //       WCard(),
-        //       WCard(),
-        //       WCard(),
-        //       WCard(),
-        //     ],
-        //   );
-        // }),
-        // body: Column(
-        //   children: [
-        //     Container(
-        //       margin: EdgeInsets.all(16),
-        //       decoration: BoxDecoration(
-        //         border: Border.all(
-        //           width: 1,
-        //           color: Colors.white,
-        //         ),
-        //         borderRadius: BorderRadius.circular(16),
-        //         boxShadow: [
-        //           BoxShadow(
-        //             color: Colors.black.withOpacity(0.2),
-        //             blurRadius: 4
-        //           )
-        //         ]
-        //       ),
-        //       child: Column(
-        //         children: [
-        //           Text(
-        //             "TITLE",
-        //             style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
-        //           ),
-        //           Text(
-        //             "Descriptionesrthkmgefwmgtrinjmewkopemjjrierkmfgeirmgiergoiemromg",
-        //           ),
-        //         ],
-        //       ),
-        //     )
-        //   ],
-        // ),
-      ),
+          // body: LayoutBuilder(builder: (context, constraints) {
+          //   print(constraints.maxHeight);
+          //   print(constraints.minHeight);
+          //   print(constraints.maxWidth);
+          //   print(constraints.minWidth);
+          //   return GridView(
+          //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          //       crossAxisCount: getMaxColumSize(constraints.maxWidth),
+          //     ),
+          //     children: [
+          //       WCard(),
+          //       WCard(),
+          //       WCard(),
+          //       WCard(),
+          //       WCard(),
+          //       WCard(),
+          //       WCard(),
+          //       WCard(),
+          //       WCard(),
+          //       WCard(),
+          //     ],
+          //   );
+          // }),
+          // body: Column(
+          //   children: [
+          //     Container(
+          //       margin: EdgeInsets.all(16),
+          //       decoration: BoxDecoration(
+          //         border: Border.all(
+          //           width: 1,
+          //           color: Colors.white,
+          //         ),
+          //         borderRadius: BorderRadius.circular(16),
+          //         boxShadow: [
+          //           BoxShadow(
+          //             color: Colors.black.withOpacity(0.2),
+          //             blurRadius: 4
+          //           )
+          //         ]
+          //       ),
+          //       child: Column(
+          //         children: [
+          //           Text(
+          //             "TITLE",
+          //             style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+          //           ),
+          //           Text(
+          //             "Descriptionesrthkmgefwmgtrinjmewkopemjjrierkmfgeirmgiergoiemromg",
+          //           ),
+          //         ],
+          //       ),
+          //     )
+          //   ],
+          // ),
+          ),
     );
   }
 }
+
 class WCard extends StatelessWidget {
   const WCard({
     super.key,
@@ -142,35 +143,34 @@ class WCard extends StatelessWidget {
   }
 }
 
-
-
-
-
-class PackManPainter extends CustomPainter{
-  final double mouthExtend ;
+class PackManPainter extends CustomPainter {
+  final double mouthExtend;
   PackManPainter({super.repaint, required this.mouthExtend});
   @override
   void paint(Canvas canvas, Size size) {
-   final centreX =  size.width/2;
-   final centreY =  size.height/2;
-   final paint = Paint();
-   paint.color = Colors.yellow;
-   canvas.drawArc(Rect.fromCenter(center: Offset(centreX,centreY), width: 100, height: 100), 0.4, 2*pi-mouthExtend, true, paint);
+    final centreX = size.width / 2;
+    final centreY = size.height / 2;
+    final paint = Paint();
+    paint.color = Colors.yellow;
+    canvas.drawArc(
+        Rect.fromCenter(
+            center: Offset(centreX, centreY), width: 100, height: 100),
+        0.4,
+        2 * pi - mouthExtend,
+        true,
+        paint);
   }
 
   @override
-  bool shouldRepaint(PackManPainter oldDelegate) =>true;
-
+  bool shouldRepaint(PackManPainter oldDelegate) => true;
 }
 
-int getMaxColumSize(double value){
-  if(value<1020){
+int getMaxColumSize(double value) {
+  if (value < 1020) {
     return 1;
-  }
-  else if(value>1020&&value<1280){
+  } else if (value > 1020 && value < 1280) {
     return 2;
-  }
-  else{
+  } else {
     return 3;
   }
 }
